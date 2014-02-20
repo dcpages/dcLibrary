8c120a57aeb104b4e15b09541605ef1b030973d4
Paul Statezny <paul@synapsestudios.com>
2014-02-19 17:21:17 -0700

    Add DocBlocks for create migration view and command.

--- src/Synapse/Command/Migrations/Create.php
+++ src/Synapse/Command/Migrations/Create.php
@@ -8,10 +8,21 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * CLI command for creating migrations
+ *
+ * Example usage:
+ *     ./console migrations:create 'Add email field to users table'
+ */
 class Create extends Command
 {
     protected $newMigrationView;
 
+    /**
+     * Set the injected new migration view, call the parent constructor
+     *
+     * @param Synapse\View\Migration\Create $newMigrationView
+     */
     public function __construct($newMigrationView)
     {
         $this->newMigrationView = $newMigrationView;
@@ -19,6 +30,9 @@ class Create extends Command
         parent::__construct();
     }
 
+    /**
+     * Set description and arguments for this console command
+     */
     protected function configure()
     {
         $this->setName('migrations:create')
@@ -30,6 +44,12 @@ class Create extends Command
             );
     }
 
+    /**
+     * Execute this console command, in order to create a new migration
+     *
+     * @param  InputInterface  $input  Command line input interface
+     * @param  OutputInterface $output Command line output interface
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $description = $input->getArgument('description');
@@ -49,6 +69,20 @@ class Create extends Command
         file_put_contents($filepath, (string) $view);
     }
 
+    /**
+     * Get the name of the new migration class.
+     * Converts description to camelCase and appends timestamp.
+     * Example:
+     *     // From:
+     *     Example description of a migration
+     *
+     *     // To:
+     *     ExampleDescriptionOfAMigration20140220001906
+     *
+     * @param  string $time        Timestamp
+     * @param  string $description User-provided description of new migration
+     * @return string
+     */
     protected function classname($time, $description)
     {
         $description = substr(strtolower($description), 0, 30);
--- src/Synapse/View/Migration/Create.php
+++ src/Synapse/View/Migration/Create.php
@@ -4,11 +4,29 @@ namespace Synapse\View\Migration;
 
 use Synapse\View\AbstractView;
 
+/**
+ * View for creating new migrations
+ */
 class Create extends AbstractView
 {
+    /**
+     * Description of the migration
+     * @var string
+     */
     protected $description;
+
+    /**
+     * Name of the migration class
+     * @var string
+     */
     protected $classname;
 
+    /**
+     * Set or get the description of the migration
+     *
+     * @param  string $description Migration description. If omitted, acts as a getter.
+     * @return string
+     */
     public function description($description = null)
     {
         if ($description === null)
@@ -17,6 +35,12 @@ class Create extends AbstractView
         $this->description = $description;
     }
 
+    /**
+     * Set or get the name of the migration class
+     *
+     * @param  string $classname Name of the migration class. If omitted, acts as a getter.
+     * @return string
+     */
     public function classname($classname = null)
     {
         if ($classname === null)
