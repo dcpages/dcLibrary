5e4db4f5760936593c9993262665f9fbc899d6d5
Paul Statezny <paul@synapsestudios.com>
2014-02-28 13:51:02 -0700

    Use traits in user mapper.
    Register service provider.
    Add missing DocBlocks.

--- src/Synapse/Mapper/User.php
+++ src/Synapse/Mapper/User.php
@@ -8,6 +8,14 @@ namespace Synapse\Mapper;
 class User extends AbstractMapper
 {
     /**
+     * Use all mapper traits, making this a general purpose mapper
+     */
+    use InserterTrait;
+    use FinderTrait;
+    use UpdaterTrait;
+    use DeleterTrait;
+
+    /**
      * Name of user table
      *
      * @var string
