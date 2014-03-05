73ac07f56c79e00fc228440d1dbc912045654cc4
Ben Youngblood <ben@synapsestudios.com>
2014-03-05 14:30:46 -0700

    synapse\application uses silex security trait

--- src/Synapse/Application.php
+++ src/Synapse/Application.php
@@ -3,12 +3,15 @@
 namespace Synapse;
 
 use Silex\Application as SilexApp;
+use Silex\Application\SecurityTrait;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\HttpFoundation\Request;
 
 class Application extends SilexApp
 {
+    use SecurityTrait;
+
     public function run(Request $request = null)
     {
         if (php_sapi_name() !== 'cli') {
