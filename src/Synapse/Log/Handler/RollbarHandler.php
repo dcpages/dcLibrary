c36801840f530fe0c176689c13d0eff20829f238
Paul Statezny <paulstatezny@gmail.com>
2014-03-06 14:48:26 -0700

    Include all data in the monolog record in the Extra Data parameter.

--- src/Synapse/Log/Handler/RollbarHandler.php
+++ src/Synapse/Log/Handler/RollbarHandler.php
@@ -40,10 +40,16 @@ class RollbarHandler extends AbstractProcessingHandler
         if (isset($record['context']['exception']) && $record['context']['exception'] instanceof Exception) {
             $this->rollbarNotifier->report_exception($record['context']['exception']);
         } else {
+            $extraData = array(
+                'level' => $record['level'],
+                'channel' => $record['channel'],
+                'datetime' => $record['datetime']->format('U'),
+            );
+
             $this->rollbarNotifier->report_message(
                 $record['message'],
                 $record['level_name'],
-                $record['extra']
+                array_merge($record['context'], $record['extra'], $extraData)
             );
         }
     }
