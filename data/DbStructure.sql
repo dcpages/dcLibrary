f453b8db9316196e68cbabede07bac1cfc197462
Paul Statezny <paulstatezny@gmail.com>
2014-03-06 16:01:08 -0700

    Add email table to DbStructure.
    Record initial app version on fresh install, so attempts to upgrade when there is no new upgrade do not try to reinstall.

--- data/DbStructure.sql
+++ data/DbStructure.sql
@@ -1,56 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (x86_64)
 -- ------------------------------------------------------
 
-CREATE TABLE oauth_clients (
-    client_id VARCHAR(80) NOT NULL,
-    client_secret VARCHAR(80) NOT NULL,
-    redirect_uri VARCHAR(2000) NOT NULL,
-    grant_types VARCHAR(80),
-    scope VARCHAR(100),
-    user_id VARCHAR(80),
-    PRIMARY KEY (client_id)
-);
-
-CREATE TABLE oauth_access_tokens (
-    access_token VARCHAR(40) NOT NULL,
-    client_id VARCHAR(80) NOT NULL,
-    user_id VARCHAR(255),
-    expires TIMESTAMP NOT NULL,
-    scope VARCHAR(2000),
-    PRIMARY KEY (access_token)
-);
-
-CREATE TABLE oauth_authorization_codes (
-    authorization_code VARCHAR(40) NOT NULL,
-    client_id VARCHAR(80) NOT NULL,
-    user_id VARCHAR(255),
-    redirect_uri VARCHAR(2000),
-    expires TIMESTAMP NOT NULL,
-    scope VARCHAR(2000),
-    PRIMARY KEY (authorization_code)
-);
-
-CREATE TABLE oauth_refresh_tokens (
-    refresh_token VARCHAR(40) NOT NULL,
-    client_id VARCHAR(80) NOT NULL,
-    user_id VARCHAR(255),
-    expires TIMESTAMP NOT NULL,
-    scope VARCHAR(2000),
-    PRIMARY KEY (refresh_token)
-);
-
-CREATE TABLE oauth_scopes (
-    scope TEXT,
-    is_default BOOLEAN
-);
-
-CREATE TABLE oauth_jwt (
-    client_id VARCHAR(80) NOT NULL,
-    subject VARCHAR(80),
-    public_key VARCHAR(2000),
-    PRIMARY KEY (client_id)
-);
-
 --
 -- Table structure for table `app_migrations`
 --
@@ -73,6 +23,129 @@ CREATE TABLE `app_versions` (
 );
 
 --
+-- Table structure for table `emails`
+--
+
+DROP TABLE IF EXISTS `emails`;
+CREATE TABLE `emails` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `hash` char(40) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `recipient_email` varchar(255) NOT NULL,
+  `recipient_name` varchar(255) DEFAULT NULL,
+  `sender` varchar(400) NOT NULL,
+  `template_name` varchar(200) DEFAULT NULL,
+  `template_data` text,
+  `message` text,
+  `bcc` varchar(255) DEFAULT NULL,
+  `attachments` text,
+  `headers` varchar(400) NOT NULL,
+  `date_sent` int(10) unsigned DEFAULT NULL,
+  `date_created` int(10) unsigned DEFAULT NULL,
+  `date_updated` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `hash` (`hash`),
+  KEY `recipient_email` (`recipient_email`),
+  KEY `status` (`status`),
+  KEY `subject` (`subject`)
+);
+
+--
+-- Table structure for table `oauth_access_tokens`
+--
+
+DROP TABLE IF EXISTS `oauth_access_tokens`;
+CREATE TABLE `oauth_access_tokens` (
+  `access_token` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`access_token`)
+);
+
+--
+-- Table structure for table `oauth_authorization_codes`
+--
+
+DROP TABLE IF EXISTS `oauth_authorization_codes`;
+CREATE TABLE `oauth_authorization_codes` (
+  `authorization_code` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `redirect_uri` varchar(2000) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`authorization_code`)
+);
+
+--
+-- Table structure for table `oauth_clients`
+--
+
+DROP TABLE IF EXISTS `oauth_clients`;
+CREATE TABLE `oauth_clients` (
+  `client_id` varchar(80) NOT NULL,
+  `client_secret` varchar(80) NOT NULL,
+  `redirect_uri` varchar(2000) NOT NULL,
+  `grant_types` varchar(80) DEFAULT NULL,
+  `scope` varchar(100) DEFAULT NULL,
+  `user_id` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+);
+
+--
+-- Table structure for table `oauth_jwt`
+--
+
+DROP TABLE IF EXISTS `oauth_jwt`;
+CREATE TABLE `oauth_jwt` (
+  `client_id` varchar(80) NOT NULL,
+  `subject` varchar(80) DEFAULT NULL,
+  `public_key` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+);
+
+--
+-- Table structure for table `oauth_refresh_tokens`
+--
+
+DROP TABLE IF EXISTS `oauth_refresh_tokens`;
+CREATE TABLE `oauth_refresh_tokens` (
+  `refresh_token` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`refresh_token`)
+);
+
+--
+-- Table structure for table `oauth_scopes`
+--
+
+DROP TABLE IF EXISTS `oauth_scopes`;
+CREATE TABLE `oauth_scopes` (
+  `scope` text,
+  `is_default` tinyint(1) DEFAULT NULL
+);
+
+--
+-- Table structure for table `pvt_roles_users`
+--
+
+DROP TABLE IF EXISTS `pvt_roles_users`;
+CREATE TABLE `pvt_roles_users` (
+  `role_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`role_id`,`user_id`),
+  KEY `fk_pvt_roles_users_user_id` (`user_id`),
+  CONSTRAINT `fk_pvt_roles_users_role_id` FOREIGN KEY (`role_id`) REFERENCES `user_roles` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_pvt_roles_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+);
+
+--
 -- Table structure for table `user_roles`
 --
 
@@ -82,22 +155,6 @@ CREATE TABLE `user_roles` (
   `name` varchar(40) NOT NULL,
   `description` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-);
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(127) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `last_login` int(10) DEFAULT NULL,
-  `created` int(10) NOT NULL,
-  `enabled` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `k_users_email_enabled` (`email`, `enabled`)
 );
 
 --
@@ -113,23 +170,25 @@ CREATE TABLE `user_tokens` (
   `created` int(10) NOT NULL,
   `expires` int(10) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_user_tokens_user_id_type` (`user_id`, `type`),
+  KEY `fk_user_tokens_user_id_type` (`user_id`,`type`),
   KEY `fk_user_tokens_expires` (`expires`),
   CONSTRAINT `fk_user_tokens_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 );
 
 --
--- Table structure for table `pvt_roles_users`
+-- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `pvt_roles_users`;
-CREATE TABLE `pvt_roles_users` (
-  `role_id` int(10) unsigned NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`role_id`, `user_id`),
-  KEY `fk_pvt_roles_users_user_id` (`user_id`),
-  CONSTRAINT `fk_pvt_roles_users_role_id` FOREIGN KEY (`role_id`) REFERENCES `user_roles` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_pvt_roles_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(127) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `last_login` int(10) DEFAULT NULL,
+  `created` int(10) NOT NULL,
+  `enabled` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `k_users_email_enabled` (`email`,`enabled`)
 );
 
--- Dump completed on 2014-02-28  0:30:22
+-- Dump completed on 2014-03-06 22:36:32
