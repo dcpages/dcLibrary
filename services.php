62022ccf0bfb24d5dc4d912dac626f3a510816d0
Ben Youngblood <ben@synapsestudios.com>
2014-03-10 12:34:03 -0700

    rename Provider\LogServiceProvider to Log\ServiceProvider

--- services.php
+++ services.php
@@ -6,8 +6,8 @@ $app->register(new Synapse\Provider\ZendDbServiceProvider());
 $app->register(new Synapse\Provider\OAuth2ServerServiceProvider());
 $app->register(new Synapse\Provider\OAuth2SecurityServiceProvider());
 $app->register(new Synapse\Provider\ResqueServiceProvider());
-$app->register(new Synapse\Provider\LogServiceProvider());
 $app->register(new Synapse\Provider\ControllerServiceProvider());
+$app->register(new Synapse\Log\ServiceProvider());
 $app->register(new Synapse\Email\ServiceProvider());
 $app->register(new Synapse\User\ServiceProvider());
 
