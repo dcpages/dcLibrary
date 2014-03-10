f962f4b604d55cda907e85e82575dbc8d055533f
Paul Statezny <paulstatezny@gmail.com>
2014-03-10 11:29:41 -0700

    Create mapper and entity for user tokens.

---
+++ src/Synapse/User/Entity/UserToken.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Synapse\User\Entity;
+
+use Synapse\Entity\AbstractEntity;
+
+/**
+ * User token entity used for registration verification among other purposes
+ */
+class UserToken extends AbstractEntity
+{
+    // Token types
+    const TYPE_REGISTRATION_VERIFICATION = 'Verify registration';
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $object = [
+        'id'      => null,
+        'user_id' => null,
+        'token'   => null,
+        'type'    => null,
+        'created' => null,
+        'expires' => null,
+    ];
+
+    /**
+     * Generate a random string to use as a token
+     *
+     * @return string
+     */
+    public function generateToken()
+    {
+        return bin2hex(openssl_random_pseudo_bytes(16));
+    }
+}
