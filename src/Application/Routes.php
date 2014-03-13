1079df96792be3bfb86a20bb9338acc2a4be3caf
Paul Statezny <paulstatezny@gmail.com>
2014-03-13 13:01:18 -0700

    Create interfaces for defining routes and registering services.
    Implement classes for application-specific routes and services.

---
+++ src/Application/Routes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Application;
+
+use Synapse\Application\RoutesInterface;
+use Synapse\Application;
+
+/**
+ * Define routes
+ */
+class Routes implements RoutesInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function define(Application $app)
+    {
+    }
+}
