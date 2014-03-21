455cf66d36866493c968e8020ccae50c9b9bf854
Paul Statezny <paulstatezny@gmail.com>
2014-03-21 10:26:29 -0700

    Add link callback route to social login config

--- config/social-login.php
+++ config/social-login.php
@@ -5,15 +5,17 @@ use OAuth\OAuth2\Service\Facebook;
 return array(
     'redirect-url' => 'http://127.0.0.1:9000/receive-token',
     'github' => array(
-        'key'            => '',
-        'secret'         => '',
-        'callback_route' => 'social-login-callback',
-        'scope'          => array('user'),
+        'key'                  => '',
+        'secret'               => '',
+        'login_callback_route' => 'social-login-callback',
+        'link_callback_route'  => 'social-link-callback',
+        'scope'                => array('user'),
     ),
     'facebook' => array(
-        'key'            => '',
-        'secret'         => '',
-        'callback_route' => 'social-login-callback',
-        'scope'          => array(Facebook::SCOPE_EMAIL, Facebook::SCOPE_READ_FRIENDLIST),
+        'key'                  => '',
+        'secret'               => '',
+        'login_callback_route' => 'social-login-callback',
+        'link_callback_route'  => 'social-link-callback',
+        'scope'                => array(Facebook::SCOPE_EMAIL, Facebook::SCOPE_READ_FRIENDLIST),
     ),
 );
