57bd1e23a37256cb5404f2b1f90ce8556ec854fe
Paul Statezny <paulstatezny@gmail.com>
2014-03-21 16:27:49 -0700

    Remove unnecessary config keys.

--- config/social-login.php
+++ config/social-login.php
@@ -5,17 +5,15 @@ use OAuth\OAuth2\Service\Facebook;
 return array(
     'redirect-url' => 'http://127.0.0.1:9000/receive-token',
     'github' => array(
-        'key'                  => '',
-        'secret'               => '',
-        'login_callback_route' => 'social-login-callback',
-        'link_callback_route'  => 'social-link-callback',
-        'scope'                => array('user'),
+        'key'            => '',
+        'secret'         => '',
+        'callback_route' => 'social-login-callback',
+        'scope'          => array('user'),
     ),
     'facebook' => array(
-        'key'                  => '',
-        'secret'               => '',
-        'login_callback_route' => 'social-login-callback',
-        'link_callback_route'  => 'social-link-callback',
-        'scope'                => array(Facebook::SCOPE_EMAIL, Facebook::SCOPE_READ_FRIENDLIST),
+        'key'            => '',
+        'secret'         => '',
+        'callback_route' => 'social-login-callback',
+        'scope'          => array(Facebook::SCOPE_EMAIL, Facebook::SCOPE_READ_FRIENDLIST),
     ),
 );
