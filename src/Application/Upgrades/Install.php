24b13a75e5ff597f3fdf745442d64f5d732bc4b6
Zachary Chavez <zach@synapsestudios.com>
2014-03-24 14:24:47 -0700

    Add record for local OAuth client in install file.

--- src/Application/Upgrades/Install.php
+++ src/Application/Upgrades/Install.php
@@ -10,5 +10,23 @@ class Install extends AbstractUpgrade
     public function execute(DbAdapter $db)
     {
         // Perform post-database-install chores here
+        $this->addLocalOAuthClient($db);
+    }
+
+    public function addLocalOAuthClient($db)
+    {
+        $connection = $db->getDriver()->getConnection();
+
+        $connection->beginTransaction();
+
+        $db->createStatement(
+            'INSERT INTO oauth_clients (client_id) VALUES (123);'
+        )->execute();
+
+        try {
+            $connection->commit();
+        } catch (\Exception $e) {
+            $connection->rollBack();
+        }
     }
 }
