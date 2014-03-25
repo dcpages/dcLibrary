6ae15cc4e3fa9eaaa34955db1bb9a1de0f66164c
Zachary Chavez <zach@synapsestudios.com>
2014-03-25 14:19:07 -0700

    Use redis session handler.

--- src/Synapse/Session/ServiceProvider.php
+++ src/Synapse/Session/ServiceProvider.php
@@ -4,15 +4,27 @@ namespace Synapse\Session;
 
 use Silex\Application;
 use Silex\ServiceProviderInterface;
-use Drak\NativeSession\NativeRedisSessionHandler;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+use Synapse\Session\RedisSessionHandler;
 
 class ServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        $app['session.storage.handler'] = $app->share(function () {
-            return new \SessionHandler('123');
+        $app['session'] = $app->share(function ($app) {
+            $app['session.storage'] = new NativeSessionStorage();
+
+            return new Session($app['session.storage']);
         });
+
+        $app['session.storage.handler'] = $app->share(function () {
+            return new RedisSessionHandler(new Redis());
+        });
+
+        $app['session.storage.options'] = array();
+        $app['session.default_locale'] = 'en';
+        $app['session.storage.save_path'] = null;
     }
 
     public function boot(Application $app)
