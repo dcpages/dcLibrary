f1afac5d12d8cc69697737655f06dc115e78cc4b
Zachary Chavez <zach@synapsestudios.com>
2014-03-25 16:58:17 -0700

    Refs #47 - Add scope config for google.

--- config/social-login.php
+++ config/social-login.php
@@ -1,19 +1,30 @@
 <?php
 
 use OAuth\OAuth2\Service\Facebook;
+use OAuth\OAuth2\Service\Google;
 
 return array(
     'redirect-url' => 'http://127.0.0.1:9000/receive-token',
-    'github' => array(
-        'key'            => '',
-        'secret'         => '',
-        'callback_route' => 'social-login-callback',
-        'scope'          => array('user'),
-    ),
+
     'facebook' => array(
         'key'            => '',
         'secret'         => '',
         'callback_route' => 'social-login-callback',
         'scope'          => array(Facebook::SCOPE_EMAIL, Facebook::SCOPE_READ_FRIENDLIST),
     ),
+
+    'github' => array(
+        'key'            => '',
+        'secret'         => '',
+        'callback_route' => 'social-login-callback',
+        'scope'          => array('user'),
+    ),
+
+    'google' => array(
+        'key'            => '',
+        'secret'         => '',
+        'callback_route' => 'social-login-callback',
+        'scope'          => [Google::SCOPE_USERINFO_EMAIL, Google::SCOPE_USERINFO_PROFILE],
+    );
+
 );
