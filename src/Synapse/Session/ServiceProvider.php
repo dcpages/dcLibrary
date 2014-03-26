fe27be07429f1438a512c0495fb54e77d35b3243
Zachary Chavez <zach@synapsestudios.com>
2014-03-26 12:03:36 -0700

    Remove unnecessary code.

--- src/Synapse/Session/ServiceProvider.php
+++ src/Synapse/Session/ServiceProvider.php
@@ -2,30 +2,18 @@
 
 namespace Synapse\Session;
 
-use Redis;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
-use Synapse\Session\RedisSessionHandler;
 
 class ServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
         $app['session'] = $app->share(function ($app) {
-            $app['session.storage'] = new NativeSessionStorage();
-
-            return new Session($app['session.storage']);
+            return new Session(new NativeSessionStorage());
         });
-
-        $app['session.storage.handler'] = $app->share(function () {
-            return new RedisSessionHandler(new Redis());
-        });
-
-        $app['session.storage.options'] = array();
-        $app['session.default_locale'] = 'en';
-        $app['session.storage.save_path'] = null;
     }
 
     public function boot(Application $app)
