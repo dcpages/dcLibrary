1de7291244500e07c73aaa1b3afadfd6226cc781
Paul Statezny <paulstatezny@gmail.com>
2014-03-27 14:22:38 -0700

    Set app version in init config.

--- config/init.php
+++ config/init.php
@@ -1,6 +1,17 @@
 <?php
 
+/**
+ * Application version
+ * Follows semantic versioning
+ *
+ * @link http://semver.org/
+ *
+ * @var string
+ */
+$appVersion = '0.0.0';
+
 return [
+    'version'    => $appVersion,
     'debug'      => false,
     'migrations' => 'Application\Migrations\\',
     'upgrades'   => 'Application\Upgrades\\',
