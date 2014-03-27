c75adb4a7cc36bc67ab546924a27cb0b623fa2d6
Paul Statezny <paulstatezny@gmail.com>
2014-03-27 09:00:25 -0700

    Delete SynapseTest suite. (Moved to base repo.)

--- tests/SynapseTest/Config/data/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array(
-    'someNotOverriddenKey' => 'notOverridden',
-    'someOverriddenKey'    => 'notOverridden',
-);
--- tests/SynapseTest/Config/data/override/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return array(
-    'someOverriddenKey'    => 'overridden',
-);
